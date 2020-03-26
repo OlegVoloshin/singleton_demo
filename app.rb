0629a8fddb9c864b4fde4f79782e7cf48b3075fe
Oleg Voloshyn <sparco2405@gmail.com>
2020-03-26 23:31:34 +0200

    Final app with pattern Singleton

--- app.rb
+++ app.rb
@@ -1,27 +1,18 @@
 #напишем какой-нибудь логгер который будет что-то логировать, затем выводить на экран, а потом в файл
+require 'singleton'
 class Logger
+
+  include Singleton
 
   def initialize
     @f = File.open("log.txt", "a")    
-  end
-
-  @@x = Logger.new # переменная класса
-
-  #создаем обьект внутри класса Logger
-  def self.instance
-    return @@x    
   end
 
  
   #instance method
   def log_something(what)
     @f.puts what
+    @f.flush #сброс на диск
   end
 
-  private_class_method :new # теперь метод .new мы можем вызывать только внутри класса
 end
-
-
-
-#logger = Logger.new  это уже не работаеи из-за privat (чтобы извне не могли создавть обьекты)
-#logger.log_something('hey!')
