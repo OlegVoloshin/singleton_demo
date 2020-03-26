7c6589dafb240f8230ef3d684c6b402ca72633b4
Oleg Voloshyn <sparco2405@gmail.com>
2020-03-26 22:00:13 +0200

    Update logger

--- app.rb
+++ app.rb
@@ -1,10 +1,21 @@
 #напишем какой-нибудь логгер который будет что-то логировать, затем выводить на экран, а потом в файл
 class Logger
 
-  def self.method_name
+  def initialize
+    @f = File.open("log.txt", "a")
+    
+  end
+  #class method
+  def self.say_something
     puts 'Hahaha'
+  end
+
+  #instance method
+  def log_something(what)
+    @f.puts what
   end
 end
 
-#logger = Logger.new
-Logger.method_name
+Logger.say_something
+logger = Logger.new
+logger.log_something('hey!')
