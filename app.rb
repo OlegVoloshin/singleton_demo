6d32d51f92124fe73ff67aef9e0e1412331f3a53
Oleg Voloshyn <sparco2405@gmail.com>
2020-03-26 16:44:41 +0200

    add static logger

---
+++ app.rb
@@ -0,0 +1,10 @@
+#напишем какой-нибудь логгер который будет что-то логировать, затем выводить на экран, а потом в файл
+class Logger
+
+  def self.method_name
+    puts 'Hahaha'
+  end
+end
+
+#logger = Logger.new
+Logger.method_name
